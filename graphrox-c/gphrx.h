71a0ceab0915bc29df0c666b96b71b3fc34f42b0
Tanner Davies <tanner.e.davies@gmail.com>
2022-11-15 19:12:04 -0700

    Finish C interface and outline some tests

--- graphrox-c/gphrx.h
+++ graphrox-c/gphrx.h
@@ -5,10 +5,8 @@
 
 #define GPHRX_ERROR_INVALID_FORMAT 0
 
-typedef int8_t bool;
-
 typedef struct {
-    void *graph_ptr;
+    const void *graph_ptr;
 } GphrxGraph;
 
 typedef struct {
@@ -17,45 +15,95 @@ typedef struct {
 } GphrxGraphEdge;
 
 typedef struct {
-    void *graph_ptr;
+    double entry;
+    uint64_t col;
+    uint64_t row;
+} GphrxMatrixEntry;
+
+typedef struct {
+    const void *graph_ptr;
 } GphrxCompressedGraph;
 
 typedef struct {
-    void *builder_ptr;
+    const void *builder_ptr;
 } GphrxCompressedGraphBuilder;
 
 typedef struct {
-    void *matrix_ptr;
+    const void *matrix_ptr;
 } GphrxCsrSquareMatrix;
 
-void free_gphrx_graph(GphrxGraph graph);
-void free_gphrx_compressed_graph(const GphrxCompressedGraph graph);
-void free_gphrx_matrix(const GphrxCsrSquareMatrix matrix);
-void free_gphrx_edge_list(const GphrxGraphEdge *list, size_t length);
+// Buffers
 void free_gphrx_string_buffer(const char *buffer);
 void free_gphrx_bytes_buffer(const unsigned char *buffer, size_t buffer_size);
 
+// Graph
+void free_gphrx_graph(GphrxGraph graph);
+void free_gphrx_edge_list(const GphrxGraphEdge *list, size_t length);
+
 GphrxGraph gphrx_new_undirected();
 GphrxGraph gphrx_new_directed();
-GphrxGraph gphrx_from_bytes(const unsigned char *buffer, size_t buffer_size, uint8_t *error);
+GphrxGraph gphrx_from_bytes(const uint8_t *buffer, size_t buffer_size, uint8_t *error);
 
 GphrxGraph gphrx_duplicate(const GphrxGraph graph);
 const char *gphrx_matrix_string(const GphrxGraph graph);
-const unsigned char *gphrx_to_bytes(const GphrxGraph graph, size_t *buffer_size);
+const uint8_t *gphrx_to_bytes(const GphrxGraph graph, size_t *buffer_size);
 
-const GphrxCsrSquareMatrix gphrx_find_avg_pool_matrix(const GphrxGraph graph, uint64_t block_dimension);
-GphrxGraph gphrx_approximate(const GphrxGraph graph, uint64_t block_dimension, double threshold);
+int8_t gphrx_is_undirected(const GphrxGraph graph);
+uint64_t gphrx_vertex_count(const GphrxGraph graph);
+uint64_t gphrx_edge_count(const GphrxGraph graph);
+int8_t gphrx_does_edge_exist(const GphrxGraph graph,
+                             uint64_t from_vertex_id,
+                             uint64_t to_vertex_id);
+
+const GphrxCsrSquareMatrix gphrx_find_avg_pool_matrix(const GphrxGraph graph,
+                                                      uint64_t block_dimension);
+GphrxGraph gphrx_approximate(const GphrxGraph graph,
+                             uint64_t block_dimension,
+                             double threshold);
 const GphrxCompressedGraph gphrx_compress(const GphrxGraph graph, double threshold);
 const GphrxGraphEdge *gphrx_get_edge_list(const GphrxGraph graph, size_t *length);
 
-bool gphrx_is_undirected(const GphrxGraph graph);
-uint64_t gphrx_vertex_count(const GphrxGraph graph);
-uint64_t gphrx_edge_count(const GphrxGraph graph);
-uint64_t gphrx_does_edge_exist(const GphrxGraph graph, uint64_t from_vertex_id, uint64_t to_vertex_id);
-
-void gphrx_add_vertex(GphrxGraph graph, uint64_t vertex_id, uint64_t *to_edges, size_t to_edges_len);
+void gphrx_add_vertex(GphrxGraph graph,
+                      uint64_t vertex_id,
+                      uint64_t *to_edges,
+                      size_t to_edges_len);
 void gphrx_add_edge(GphrxGraph graph, uint64_t from_vertex_id, uint64_t to_vertex_id);
 void gphrx_delete_edge(GphrxGraph graph, uint64_t from_vertex_id, uint64_t to_vertex_id);
+
+// CompressedGraph
+void free_gphrx_compressed_graph(const GphrxCompressedGraph graph);
+
+double gphrx_compressed_graph_threshold(const GphrxCompressedGraph graph);
+int8_t gphrx_compressed_graph_is_undirected(const GphrxCompressedGraph graph);
+uint64_t gphrx_compressed_graph_vertex_count(const GphrxCompressedGraph graph);
+uint64_t gphrx_compressed_graph_edge_count(const GphrxCompressedGraph graph);
+
+int8_t gphrx_compressed_graph_does_edge_exist(const GphrxCompressedGraph graph,
+                                              uint64_t from_vertex_id,
+                                              uint64_t to_vertex_id);
+uint64_t gphrx_get_compressed_matrix_entry(const GphrxCompressedGraph graph,
+                                           uint64_t col,
+                                           uint64_t row);
+const char *gphrx_compressed_graph_matrix_representation_string(const GphrxCompressedGraph);
+const uint8_t *gphrx_compressed_graph_to_bytes(const GphrxCompressedGraph,
+                                               size_t *buffer_size);
+GphrxCompressedGraph gphrx_compressed_graph_from_bytes(const uint8_t *buffer,
+                                                       size_t buffer_size,
+                                                       uint8_t *error);
+GphrxGraph gphrx_decompress(const GphrxCompressedGraph graph);
+
+// Matrix
+void free_gphrx_matrix(const GphrxCsrSquareMatrix matrix);
+void free_gphrx_matrix_entry_list(const GphrxMatrixEntry *list, size_t length);
+
+uint64_t gphrx_matrix_dimension(const GphrxCsrSquareMatrix matrix);
+uint64_t gphrx_matrix_entry_count(const GphrxCsrSquareMatrix matrix);
+
+const char *gphrx_matrix_to_string(const GphrxCsrSquareMatrix matrix);
+const char *gphrx_matrix_to_string_with_precision(const GphrxCsrSquareMatrix matrix,
+                                                  size_t decimal_digits);
+const GphrxMatrixEntry *gphrx_matrix_get_entry_list(const GphrxCsrSquareMatrix matrix,
+                                                    size_t *length);
 
 #define __GPHRX_H
 #endif
--- graphrox-c/src/lib.rs
+++ graphrox-c/src/lib.rs
@@ -1,15 +1,4 @@
-// TODO
-// - Fill in functions
-// - Add duplicate function
-// - Add function to get a list of tuples with (entry, col, row) for Python
-
-// TODO: Make headerfile, include documentation in header file
-
-// TODO: Don't allow unused imports
-#![allow(unused_imports)]
-
-use graphrox::error::GraphRoxError;
-use graphrox::matrix::{CsrAdjacencyMatrix, CsrSquareMatrix};
+use graphrox::matrix::{CsrSquareMatrix, MatrixRepresentation};
 use graphrox::{CompressedGraph, Graph, GraphRepresentation};
 
 use std::ffi;
@@ -32,6 +21,13 @@ pub struct GphrxGraph {
 
 #[repr(C)]
 pub struct GphrxGraphEdge {
+    pub col: u64,
+    pub row: u64,
+}
+
+#[repr(C)]
+pub struct GphrxMatrixEntry {
+    pub entry: f64,
     pub col: u64,
     pub row: u64,
 }
@@ -64,11 +60,18 @@ pub unsafe extern "C" fn free_gphrx_compressed_graph(graph: GphrxCompressedGraph
 #[no_mangle]
 pub unsafe extern "C" fn free_gphrx_matrix(matrix: GphrxCsrSquareMatrix) {
     // This module only exposes an average pool matrix, which is of type CsrSquareMatrix<f64>
-    drop(Box::from_raw(matrix.matrix_ptr as *mut CsrSquareMatrix<f64>));
+    drop(Box::from_raw(
+        matrix.matrix_ptr as *mut CsrSquareMatrix<f64>,
+    ));
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn free_gphrx_edge_list(list: *const GphrxGraphEdge, length: usize) {
+    drop(slice::from_raw_parts(list, length));
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn free_gphrx_matrix_entry_list(list: *const GphrxMatrixEntry, length: usize) {
     drop(slice::from_raw_parts(list, length));
 }
 
@@ -78,7 +81,7 @@ pub unsafe extern "C" fn free_gphrx_string_buffer(buffer: *mut ffi::c_char) {
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn free_gphrx_bytes_buffer(buffer: *mut ffi::c_uchar, buffer_size: usize) {
+pub unsafe extern "C" fn free_gphrx_bytes_buffer(buffer: *mut u8, buffer_size: usize) {
     drop(slice::from_raw_parts(buffer, buffer_size));
 }
 
@@ -198,10 +201,7 @@ pub unsafe extern "C" fn gphrx_does_edge_exist(
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn gphrx_to_bytes(
-    graph: GphrxGraph,
-    buffer_size: *mut usize,
-) -> *const ffi::c_uchar {
+pub unsafe extern "C" fn gphrx_to_bytes(graph: GphrxGraph, buffer_size: *mut usize) -> *const u8 {
     let graph = (graph.graph_ptr as *const Graph)
         .as_ref()
         .unwrap_unchecked();
@@ -269,10 +269,7 @@ pub unsafe extern "C" fn gphrx_approximate(
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn gphrx_compress(
-    graph: GphrxGraph,
-    threshold: f64,
-) -> GphrxCompressedGraph {
+pub unsafe extern "C" fn gphrx_compress(graph: GphrxGraph, threshold: f64) -> GphrxCompressedGraph {
     let graph = (graph.graph_ptr as *const Graph)
         .as_ref()
         .unwrap_unchecked();
@@ -312,6 +309,412 @@ pub unsafe extern "C" fn gphrx_get_edge_list(
     buffer_ptr as *const GphrxGraphEdge
 }
 
-// TODO: CsrSquareMatrix<f64>, non-mutating methods only (including iterator)
-// TODO: CompressedGraph
-// TODO: CompressedGraphBuilder
+#[no_mangle]
+pub unsafe extern "C" fn gphrx_matrix_dimension(matrix: GphrxCsrSquareMatrix) -> u64 {
+    let matrix = (matrix.matrix_ptr as *const CsrSquareMatrix<f64>)
+        .as_ref()
+        .unwrap_unchecked();
+
+    matrix.dimension()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gphrx_matrix_entry_count(matrix: GphrxCsrSquareMatrix) -> u64 {
+    let matrix = (matrix.matrix_ptr as *const CsrSquareMatrix<f64>)
+        .as_ref()
+        .unwrap_unchecked();
+
+    matrix.entry_count()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gphrx_matrix_to_string(
+    matrix: GphrxCsrSquareMatrix,
+) -> *const ffi::c_char {
+    let matrix = (matrix.matrix_ptr as *const CsrSquareMatrix<f64>)
+        .as_ref()
+        .unwrap_unchecked();
+
+    ffi::CString::from_vec_unchecked((*matrix).to_string().as_bytes().to_vec()).into_raw()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gphrx_matrix_to_string_with_precision(
+    matrix: GphrxCsrSquareMatrix,
+    decimal_digits: usize,
+) -> *const ffi::c_char {
+    let matrix = (matrix.matrix_ptr as *const CsrSquareMatrix<f64>)
+        .as_ref()
+        .unwrap_unchecked();
+
+    ffi::CString::from_vec_unchecked(
+        (*matrix)
+            .to_string_with_precision(decimal_digits)
+            .as_bytes()
+            .to_vec(),
+    )
+    .into_raw()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gphrx_matrix_get_entry_list(
+    matrix: GphrxCsrSquareMatrix,
+    length: *mut usize,
+) -> *const GphrxMatrixEntry {
+    let matrix = (matrix.matrix_ptr as *const CsrSquareMatrix<f64>)
+        .as_ref()
+        .unwrap_unchecked();
+
+    let mut buffer = mem::MaybeUninit::new(Vec::with_capacity(matrix.entry_count() as usize));
+    *length = matrix.entry_count() as usize;
+
+    let buffer_ptr = unsafe {
+        (*buffer.as_mut_ptr()).set_len((*buffer.as_mut_ptr()).capacity());
+        (*buffer.as_mut_ptr()).as_mut_ptr() as *mut GphrxMatrixEntry
+    };
+
+    let mut pos = 0;
+
+    for (entry, col, row) in matrix {
+        let entry = GphrxMatrixEntry { entry, col, row };
+        ptr::write(buffer_ptr.add(pos), entry);
+        pos += 1;
+    }
+
+    buffer_ptr as *const GphrxMatrixEntry
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gphrx_compressed_graph_threshold(graph: GphrxCompressedGraph) -> f64 {
+    let graph = (graph.graph_ptr as *const CompressedGraph)
+        .as_ref()
+        .unwrap_unchecked();
+
+    graph.threshold()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gphrx_compressed_graph_is_undirected(
+    graph: GphrxCompressedGraph,
+) -> CBool {
+    let graph = (graph.graph_ptr as *const CompressedGraph)
+        .as_ref()
+        .unwrap_unchecked();
+
+    if graph.is_undirected() {
+        C_TRUE
+    } else {
+        C_FALSE
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gphrx_compressed_graph_vertex_count(graph: GphrxCompressedGraph) -> u64 {
+    let graph = (graph.graph_ptr as *const CompressedGraph)
+        .as_ref()
+        .unwrap_unchecked();
+
+    graph.vertex_count()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gphrx_compressed_graph_edge_count(graph: GphrxCompressedGraph) -> u64 {
+    let graph = (graph.graph_ptr as *const CompressedGraph)
+        .as_ref()
+        .unwrap_unchecked();
+
+    graph.edge_count()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gphrx_compressed_graph_does_edge_exist(
+    graph: GphrxCompressedGraph,
+    from_vertex_id: u64,
+    to_vertex_id: u64,
+) -> CBool {
+    let graph = (graph.graph_ptr as *const CompressedGraph)
+        .as_ref()
+        .unwrap_unchecked();
+
+    if graph.does_edge_exist(from_vertex_id, to_vertex_id) {
+        C_TRUE
+    } else {
+        C_FALSE
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gphrx_get_compressed_matrix_entry(
+    graph: GphrxCompressedGraph,
+    col: u64,
+    row: u64,
+) -> u64 {
+    let graph = (graph.graph_ptr as *const CompressedGraph)
+        .as_ref()
+        .unwrap_unchecked();
+
+    graph.get_compressed_matrix_entry(col, row)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gphrx_compressed_graph_matrix_representation_string(
+    graph: GphrxCompressedGraph,
+) -> *const ffi::c_char {
+    let graph = (graph.graph_ptr as *const CompressedGraph)
+        .as_ref()
+        .unwrap_unchecked();
+
+    ffi::CString::from_vec_unchecked((*graph).matrix_representation_string().as_bytes().to_vec())
+        .into_raw()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gphrx_compressed_graph_to_bytes(
+    graph: GphrxCompressedGraph,
+    buffer_size: *mut usize,
+) -> *const u8 {
+    let graph = (graph.graph_ptr as *const CompressedGraph)
+        .as_ref()
+        .unwrap_unchecked();
+
+    let buffer = mem::ManuallyDrop::new(graph.to_bytes());
+    *buffer_size = buffer.len();
+
+    buffer.as_ptr()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gphrx_compressed_graph_from_bytes(
+    buffer: *const u8,
+    buffer_size: usize,
+    error: *mut u8,
+) -> GphrxCompressedGraph {
+    let buffer = mem::ManuallyDrop::new(slice::from_raw_parts(buffer, buffer_size));
+
+    let graph = match CompressedGraph::try_from(*buffer) {
+        Ok(b) => b,
+        Err(_) => {
+            *error = GPHRX_ERROR_INVALID_FORMAT;
+            return GphrxCompressedGraph {
+                graph_ptr: ptr::null_mut(),
+            };
+        }
+    };
+
+    GphrxCompressedGraph {
+        graph_ptr: Box::into_raw(Box::new(graph)) as *mut ffi::c_void,
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gphrx_decompress(graph: GphrxCompressedGraph) -> GphrxGraph {
+    let graph = (graph.graph_ptr as *const CompressedGraph)
+        .as_ref()
+        .unwrap_unchecked();
+
+    let graph = graph.decompress();
+
+    GphrxGraph {
+        graph_ptr: Box::into_raw(Box::new(graph)) as *mut ffi::c_void,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+
+    // #[test]
+    // fn test_free_gphrx_string_buffer() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_free_gphrx_bytes_buffer() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_free_gphrx_graph() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_free_gphrx_edge_list() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_new_undirected() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_new_directed() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_from_bytes() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_duplicate() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_matrix_string() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_to_bytes() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_is_undirected() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_vertex_count() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_edge_count() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_does_edge_exist() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_find_avg_pool_matrix() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_approximate() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_compress() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_get_edge_list() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_add_vertex() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_add_edge() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_delete_edge() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_free_gphrx_compressed_graph() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_compressed_graph_threshold() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_compressed_graph_is_undirected() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_compressed_graph_vertex_count() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_compressed_graph_edge_count() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_compressed_graph_does_edge_exist() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_get_compressed_matrix_entry() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_compressed_graph_matrix_representation_string() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_compressed_graph_to_bytes() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_compressed_graph_from_bytes() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_decompress() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_free_gphrx_matrix() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_free_gphrx_matrix_entry_list() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_matrix_dimension() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_matrix_entry_count() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_matrix_to_string() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_matrix_to_string_with_precision() {
+    //     todo!();
+    // }
+
+    // #[test]
+    // fn test_gphrx_matrix_get_entry_list() {
+    //     todo!();
+    // }
+}
