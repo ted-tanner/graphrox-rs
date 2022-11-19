0319575a8b1e9bdb44fe9fbfcbbd56e578f6a0da
Tanner Davies <tanner.e.davies@gmail.com>
2022-11-19 11:05:05 -0700

    Finish Python interface

--- graphrox-py/graphrox/__init__.py
+++ graphrox-py/graphrox/__init__.py
@@ -70,7 +70,8 @@ class _GphrxCsrSquareMatrix_c(ctypes.Structure):
     ]
 
 
-class _GphrxErrorCode(enum.Enum):
+class _GphrxError(enum.Enum):
+    GPHRX_NO_ERROR = 0
     GPHRX_ERROR_INVALID_FORMAT = 1
 
 
@@ -223,52 +224,125 @@ _lib.gphrx_matrix_get_entry_list.restype = ctypes.POINTER(_GphrxMatrixEntry_c)
 
 
 class Graph:
-    pass
-
-
-class CompressedGraph:
-    pass
-
-
-class CsrSquareMatrix:
-    def __init__(self, c_csr_matrix):
-        if not isinstance(c_csr_matrix, _GphrxCsrSquareMatrix_c):
-            raise TypeError('Provided matrix was of the wrong type')
+    def __init__(self, is_undirected=False, c_graph=None):
+        if c_graph_obj is not None:
+            if not isinstance(c_graph_obj, _GphrxGraph_c):
+                raise TypeError('provided item was of the wrong type')
+            self._graph = c_graph_obj
+        else:
+            self._graph = gphrx_new_undirected() if is_undirected else gphrx_new_directed()
         
-        self._matrix = c_csr_matrix
-
     def __del__(self):
-        _lib.free_gphrx_matrix(self._matrix)
+        _lib.free_gphrx_graph(self._graph)
+
+    def __bytes__(self):
+        size = ctypes.c_size_t()
+        bytes_ptr = _lib.gphrx_to_bytes(self._graph, ctypes.byref(size))
+        py_bytes = bytes(bytes_ptr[:size.value])
+
+        _lib.free_gphrx_bytes_buffer(bytes_ptr)
+
+        return py_bytes
+
+    @staticmethod
+    def from_bytes(bytes_obj):
+        bytes_arr = (ctypes.c_ubyte * (len(bytes_obj))).from_buffer(bytearray(bytes_obj))
+
+        error = ctypes.c_uint8()
+        c_graph = _lib.gphrx_from_bytes(bytes_arr,
+                                        len(bytes_obj),
+                                        ctypes.byref(error))
+
+        if error.value != _GphrxError.GPHRX_NO_ERROR.value:
+            raise ValueError(Graph.__name__() + ' could not be constructed from the provided bytes')
+
+        return Graph(c_graph=c_graph)
 
     def duplicate(self):
-        c_matrix = _lib.gphrx_matrix_duplicate(self._matrix)
-        return CsrSquareMatrix(c_matrix)
+        c_graph = _lib.gphrx_duplicate(self._graph)
+        return Graph(c_graph=c_graph)
 
-    def dimension(self):
-        return int(_lib.gphrx_matrix_dimension(self._matrix))
-
-    def entry_count(self):
-        return int(_lib.gphrx_matrix_entry_count(self._matrix))
-
-    def get_entry(self, col, row):
-        return float(_lib.gphrx_matrix_get_entry(self._matrix, col, row))
-
-    def to_string(self, decimal_digits=2):
-        c_str = _lib.gphrx_matrix_to_string_with_precision(self._matrix, decimal_digits)
+    def matrix_string(self):
+        c_str = _lib.gphrx_matrix_string(self._graph)
         py_str = ctypes.cast(c_str, ctypes.c_char_p).value
         _lib.free_gphrx_string_buffer(c_str)
         return py_str.decode('utf-8')
 
-    def __str__(self):
-        return self.to_string()
+    def is_undirected(self):
+        return bool(_lib.gphrx_is_undirected(self._graph).value)
 
-    def entry_list(self):
+    def vertex_count(self):
+        return int(_lib.gphrx_vertex_count(self._graph).value)
+
+    def edge_count(self):
+        return int(_lib.gphrx_edge_count(self._graph).value)
+
+    def does_edge_exist(self, from_vertex_id, to_vertex_id):
+        return bool(_lib.gphrx_does_edge_exist(self._graph,
+                                               from_vertex_id,
+                                               to_vertex_id).value)
+
+    def add_vertex(self, vertex_id, to_edges=None):
+        if to_edges is None or len(to_edges) == 0:
+            _lib.gphrx_add_vertex(self._graph, vertex_id, None, 0)
+        else:
+            EdgesArray = ctypes.c_uint64 * len(to_edges)
+            edges = EdgesArray(*to_edges)
+            _lib.gphrx_add_vertex(self._graph, vertex_id, edges, len(to_edges))
+
+    def add_edge(self, from_vertex_id, to_vertex_id):
+        _lib.gphrx_add_edge(self._graph, from_vertex_id, to_vertex_id)
+
+    def delete_edge(self, from_vertex_id, to_vertex_id):
+        _lib.gphrx_delete_edge(self._graph, from_vertex_id, to_vertex_id)
+
+    def find_avg_pool_matrix(self, block_dimension):
+        c_matrix = _lib.gphrx_find_avg_pool_matrix(self._graph, block_dimension)
+        return CsrSquareMatrix(c_matrix)
+
+    def approximate(self, block_dimension, threshold):
+        c_graph = _lib.gphrx_approximate(self._graph, block_dimension, threshold)
+        return Graph(c_graph=c_graph)
+
+    def compress(self, threshold):
+        c_compressed_graph = _lib.gphrx_compress(self._graph, threshold)
+        return CompressedGraph(c_compressed_graph)
+
+    def edge_list(self):
         size = ctypes.c_size_t()
-        arr_ptr = _lib.gphrx_matrix_get_entry_list(self._matrix, ctypes.byref(size))
-        return CsrSquareMatrixEntryList(arr_ptr, size)
+        arr_ptr = _lib.gphrx_get_edge_list(self._graph, ctypes.byref(size))
+        return GraphEdgeList(arr_ptr, size.value)
 
-    
-class CsrSquareMatrixEntryListIterator:
+
+class GraphEdgeList:
+    def __init__(self, c_list_ptr, size):
+        if not isinstance(c_list_ptr, ctypes.POINTER):
+            raise TypeError(type(self).__name__ + ' received a non-pointer object')
+
+        self._ptr = c_list_ptr
+        self._size = size
+
+    def __del__(self):
+        _lib.free_gphrx_edge_list(self._ptr, self._size)
+
+    def __getitem__(self, idx):
+        if idx >= self._size:
+            raise IndexError('list index out of range')
+
+        if idx < 0:
+            idx = self._size - max(Math.abs(idx) % self._size, 1)
+
+        item = self._ptr[idx]
+        return (int(item.col.value), int(item.row.value))
+
+    def __iter__(self):
+        return GraphEdgeListIterator(self._ptr, self._size)
+
+    def __len__(self):
+        return int(self._size)
+
+
+class GraphEdgeListIterator:
     def __init__(self, c_list_ptr, size):
         if not isinstance(c_list_ptr, ctypes.POINTER):
             raise TypeError(type(self).__name__ + ' received a non-pointer object')
@@ -284,7 +358,114 @@ class CsrSquareMatrixEntryListIterator:
             curr = self._ptr[self._pos]
             self._pos += 1
 
-            return (float(curr.entry), int(curr.col), int(curr.row))
+            return (int(curr.col.value), int(curr.row.value))
+
+
+class CompressedGraph:
+    def __init__(self, c_compressed_graph):
+        if not isinstance(c_compressed_graph, _GphrxCompressedGraph_c):
+            raise TypeError('provided item was of the wrong type')
+        
+        self._graph = c_compressed_graph
+
+    def __del__(self):
+        _lib.free_gphrx_compressed_graph(self._graph)
+
+    def __bytes__(self):
+        size = ctypes.c_size_t()
+        bytes_ptr = _lib.gphrx_compressed_graph_to_bytes(self._graph, ctypes.byref(size))
+        py_bytes = bytes(bytes_ptr[:size.value])
+
+        _lib.free_gphrx_bytes_buffer(bytes_ptr)
+
+        return py_bytes
+
+    @staticmethod
+    def from_bytes(bytes_obj):
+        bytes_arr = (ctypes.c_ubyte * (len(bytes_obj))).from_buffer(bytearray(bytes_obj))
+
+        error = ctypes.c_uint8()
+        c_graph = _lib.gphrx_compressed_graph_from_bytes(bytes_arr,
+                                                         len(bytes_obj),
+                                                         ctypes.byref(error))
+
+        if error.value != _GphrxError.GPHRX_NO_ERROR.value:
+            raise ValueError(CompressedGraph.__name__() + ' could not be constructed from the provided bytes')
+
+        return CompressedGraph(c_graph)
+
+    def duplicate(self):
+        c_graph = _lib.gphrx_compressed_graph_duplicate(self._graph)
+        return CompressedGraph(c_graph)
+
+    def matrix_string(self):
+        c_str = _lib.gphrx_compressed_graph_matrix_string(self._graph)
+        py_str = ctypes.cast(c_str, ctypes.c_char_p).value
+        _lib.free_gphrx_string_buffer(c_str)
+        return py_str.decode('utf-8')
+
+    def decompress(self):
+        c_graph = gphrx_decompress(self._graph)
+        return Graph(c_graph=c_graph)
+
+    def threshold(self):
+        return int(_lib.gphrx_compressed_graph_threshold(self._graph).value)
+
+    def is_undirected(self):
+        return bool(_lib.gphrx_compressed_graph_is_undirected(self._graph).value)
+
+    def vertex_count(self):
+        return int(_lib.gphrx_compressed_graph_vertex_count(self._graph).value)
+
+    def edge_count(self):
+        return int(_lib.gphrx_compressed_graph_edge_count(self._graph).value)
+
+    def does_edge_exist(self, from_vertex_id, to_vertex_id):
+        return bool(_lib.gphrx_compressed_graph_does_edge_exist(self._graph,
+                                                                from_vertex_id,
+                                                                to_vertex_id).value)
+
+    def get_compressed_matrix_entry(self, col, row):
+        return int(_lib.gphrx_get_compressed_matrix_entry(self._graph, col, row).value)
+        
+
+
+class CsrSquareMatrix:
+    def __init__(self, c_csr_matrix):
+        if not isinstance(c_csr_matrix, _GphrxCsrSquareMatrix_c):
+            raise TypeError('provided item was of the wrong type')
+        
+        self._matrix = c_csr_matrix
+
+    def __del__(self):
+        _lib.free_gphrx_matrix(self._matrix)
+
+    def __str__(self):
+        return self.to_string()
+
+    def duplicate(self):
+        c_matrix = _lib.gphrx_matrix_duplicate(self._matrix)
+        return CsrSquareMatrix(c_matrix)
+
+    def dimension(self):
+        return int(_lib.gphrx_matrix_dimension(self._matrix).value)
+
+    def entry_count(self):
+        return int(_lib.gphrx_matrix_entry_count(self._matrix).value)
+
+    def get_entry(self, col, row):
+        return float(_lib.gphrx_matrix_get_entry(self._matrix, col, row).value)
+
+    def to_string(self, decimal_digits=2):
+        c_str = _lib.gphrx_matrix_to_string_with_precision(self._matrix, decimal_digits)
+        py_str = ctypes.cast(c_str, ctypes.c_char_p).value
+        _lib.free_gphrx_string_buffer(c_str)
+        return py_str.decode('utf-8')
+
+    def entry_list(self):
+        size = ctypes.c_size_t()
+        arr_ptr = _lib.gphrx_matrix_get_entry_list(self._matrix, ctypes.byref(size))
+        return CsrSquareMatrixEntryList(arr_ptr, size.value)
     
 
 class CsrSquareMatrixEntryList:
@@ -306,10 +487,29 @@ class CsrSquareMatrixEntryList:
             idx = self._size - max(Math.abs(idx) % self._size, 1)
 
         item = self._ptr[idx]
-        return (float(item.entry), int(item.col), int(item.row))
+        return (float(item.entry.value), int(item.col.value), int(item.row.value))
 
     def __iter__(self):
         return CsrSquareMatrixEntryListIterator(self._ptr, self._size)
 
     def __len__(self):
         return int(self._size)
+
+
+class CsrSquareMatrixEntryListIterator:
+    def __init__(self, c_list_ptr, size):
+        if not isinstance(c_list_ptr, ctypes.POINTER):
+            raise TypeError(type(self).__name__ + ' received a non-pointer object')
+
+        self._ptr = c_list_ptr
+        self._size = size
+        self._pos = 0
+
+    def __next__(self):
+        if self._pos == self._size:
+            raise StopIteration
+        else:
+            curr = self._ptr[self._pos]
+            self._pos += 1
+
+            return (float(curr.entry.value), int(curr.col.value), int(curr.row.value))
